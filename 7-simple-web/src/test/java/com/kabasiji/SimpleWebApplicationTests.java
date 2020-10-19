7e82375024420630dad86b91e6b0a78bd079e22a
kjhuang <kjhuang@streamax.com>
2020-10-19 16:19:55 +0800

    新增一个简单的web初始模块，用于简单的web接口测试使用

---
+++ 7-simple-web/src/test/java/com/kabasiji/SimpleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kabasiji;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebApplicationTests {
+
+     @Test
+     void contextLoads() {
+     }
+
+}
